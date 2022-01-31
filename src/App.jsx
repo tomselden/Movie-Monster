9044c2156e33c22f34333ebb882fa215816b7616
Thomas Selden <thomasselden42@gmail.com>
2022-01-31 18:40:52 -0500

    activity movie monster

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import DisplayIdeas from "./DisplayIdeas";
+import IdeasForm from "./IdeasForm"
 
 function App() {
   const [ideas, setIdeas] = useState([
@@ -8,20 +9,19 @@ function App() {
     "Horror film where AWS goes down for 24 hours"
   ]);
 
-
+  const addIdeas = (newIdeas) => {
+    setIdeas([...ideas, newIdeas])
+  }
 
   return (
     <main>
       <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
 
-      <br />
-      
-      <h1>Submit your ideas down below:</h1>
-
-
+      <DisplayIdeas ideas={ideas} />
+      <h1>Submit your ideas down below</h1>
+      <IdeasForm addIdeas={addIdeas} />
     </main>
   );
 }
 
 export default App;
-
